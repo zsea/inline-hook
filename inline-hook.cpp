db8b7fb232f73431a9546e23e1019eda5ebfd098
曾大海 <zsea@qq.com>
2021-05-08 17:23:03 +0800

    添加内存申请校验

--- inline-hook.cpp
+++ inline-hook.cpp
@@ -4,9 +4,14 @@ HHANDLE* InlineHook(DWORD hookAddr, HHooker handler, DWORD originLength)
 {
 	if (originLength < 5) return NULL;
 	HHANDLE* hHandle = (HHANDLE*)malloc(sizeof(HHANDLE));
+	if (hHandle == NULL) return NULL;
 	memset(hHandle, 0, sizeof(HHANDLE));
 	hHandle->address = hookAddr;
-	hHandle->origin = (BYTE*)malloc(sizeof(BYTE)*originLength);
+	hHandle->origin = (BYTE*)malloc(sizeof(BYTE) * originLength);
+	if (hHandle->origin == NULL) {
+		free(hHandle);
+		return NULL;
+	}
 	hHandle->codeLength = originLength;
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, GetCurrentProcessId());
 	if (hProcess == INVALID_HANDLE_VALUE) {
@@ -15,21 +20,26 @@ HHANDLE* InlineHook(DWORD hookAddr, HHooker handler, DWORD originLength)
 		return NULL;
 	}
 	DWORD jmpAddr = ((DWORD)handler) - (hookAddr + 5);
-	BYTE *jmpCode = (BYTE*)malloc(sizeof(BYTE)*originLength);
+	BYTE* jmpCode = (BYTE*)malloc(sizeof(BYTE) * originLength);
+	if (jmpCode == NULL) {
+		free(hHandle->origin);
+		free(hHandle);
+		return NULL;
+	}
 	*(jmpCode + 0) = 0xE8;
-	*(DWORD *)(jmpCode + 1) = jmpAddr;
+	*(DWORD*)(jmpCode + 1) = jmpAddr;
 	for (int i = 5; i < originLength; i++) {
 		*(jmpCode + i) = 0x90;
 	}
 
-	if (ReadProcessMemory(hProcess, (LPVOID)hookAddr, hHandle->origin, originLength, NULL) == 0) {
+	if (ReadProcessMemory(hProcess, (LPVOID)hookAddr, hHandle->origin, originLength, NULL) == FALSE) {
 		OutputDebugString(L"[InlineHook] read code fail.");
 		FreeHHandle(hHandle);
 		::CloseHandle(hProcess);
 		free(jmpCode);
 		return NULL;
 	}
-	if (WriteProcessMemory(hProcess, (LPVOID)hookAddr, jmpCode, originLength, NULL) == 0) {
+	if (WriteProcessMemory(hProcess, (LPVOID)hookAddr, jmpCode, originLength, NULL) == FALSE) {
 		OutputDebugString(L"[InlineHook] write code fail.");
 		FreeHHandle(hHandle);
 		::CloseHandle(hProcess);
@@ -41,7 +51,7 @@ HHANDLE* InlineHook(DWORD hookAddr, HHooker handler, DWORD originLength)
 	return hHandle;
 }
 
-BOOL InlineUnHook(HHANDLE * hHandle)
+BOOL InlineUnHook(HHANDLE* hHandle)
 {
 
 	if (hHandle == NULL) return FALSE;
@@ -58,14 +68,14 @@ BOOL InlineUnHook(HHANDLE * hHandle)
 	return TRUE;
 }
 
-DWORD GetHookAddressW(const wchar_t * model, DWORD offset)
+DWORD GetHookAddressW(const wchar_t* model, DWORD offset)
 {
 	return (DWORD)GetModuleHandleW(model) + offset;
 }
-DWORD GetHookAddressA(const char * model, DWORD offset) {
+DWORD GetHookAddressA(const char* model, DWORD offset) {
 	return (DWORD)GetModuleHandleA(model) + offset;
 }
-void FreeHHandle(HHANDLE * hHandle)
+void FreeHHandle(HHANDLE* hHandle)
 {
 	if (hHandle == NULL) return;
 	free(hHandle->origin);
